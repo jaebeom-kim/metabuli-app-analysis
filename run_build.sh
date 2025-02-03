a2288e195ae66db6321e9638f990371f5a038b2e
Jaebeom Kim <jbeom0731@gmail.com>
2025-02-03 12:23:43 +0900

    test

--- run_build.sh
+++ run_build.sh
@@ -17,7 +17,7 @@ echo $THREADS
 
 # Run build five time and redirect stdout and stderr of each run to separate files
 $METABULI_PATH build $DBDIR $DBDIR/downloaded_files.txt $GTDB_TAXDUMP/taxid.map --taxonomy-path $GTDB_TAXDUMP --gtdb 1 --threads $THREADS --max-ram $MAXRAM --make-library 0 > $DBDIR/build1.log 2> $DBDIR/build1.err
-#$METABULI_PATH build $DBDIR $DBDIR/downloaded_files.txt $GTDB_TAXDUMP/taxid.map --taxonomy-path $GTDB_TAXDUMP --gtdb 1 --threads $THREADS --max-ram $MAXRAM --make-library 0 > $DBDIR/build2.log 2> $DBDIR/build2.err
-#$METABULI_PATH build $DBDIR $DBDIR/downloaded_files.txt $GTDB_TAXDUMP/taxid.map --taxonomy-path $GTDB_TAXDUMP --gtdb 1 --threads $THREADS --max-ram $MAXRAM --make-library 0 > $DBDIR/build3.log 2> $DBDIR/build3.err
-#$METABULI_PATH build $DBDIR $DBDIR/downloaded_files.txt $GTDB_TAXDUMP/taxid.map --taxonomy-path $GTDB_TAXDUMP --gtdb 1 --threads $THREADS --max-ram $MAXRAM --make-library 0 > $DBDIR/build4.log 2> $DBDIR/build4.err
-#$METABULI_PATH build $DBDIR $DBDIR/downloaded_files.txt $GTDB_TAXDUMP/taxid.map --taxonomy-path $GTDB_TAXDUMP --gtdb 1 --threads $THREADS --max-ram $MAXRAM --make-library 0 > $DBDIR/build5.log 2> $DBDIR/build5.err
+$METABULI_PATH build $DBDIR $DBDIR/downloaded_files.txt $GTDB_TAXDUMP/taxid.map --taxonomy-path $GTDB_TAXDUMP --gtdb 1 --threads $THREADS --max-ram $MAXRAM --make-library 0 > $DBDIR/build2.log 2> $DBDIR/build2.err
+$METABULI_PATH build $DBDIR $DBDIR/downloaded_files.txt $GTDB_TAXDUMP/taxid.map --taxonomy-path $GTDB_TAXDUMP --gtdb 1 --threads $THREADS --max-ram $MAXRAM --make-library 0 > $DBDIR/build3.log 2> $DBDIR/build3.err
+$METABULI_PATH build $DBDIR $DBDIR/downloaded_files.txt $GTDB_TAXDUMP/taxid.map --taxonomy-path $GTDB_TAXDUMP --gtdb 1 --threads $THREADS --max-ram $MAXRAM --make-library 0 > $DBDIR/build4.log 2> $DBDIR/build4.err
+$METABULI_PATH build $DBDIR $DBDIR/downloaded_files.txt $GTDB_TAXDUMP/taxid.map --taxonomy-path $GTDB_TAXDUMP --gtdb 1 --threads $THREADS --max-ram $MAXRAM --make-library 0 > $DBDIR/build5.log 2> $DBDIR/build5.err
